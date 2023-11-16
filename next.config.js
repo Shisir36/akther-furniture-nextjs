ba312ede3063678c213db45712168b30628ad699
al mamun <almamunshisir770@gmail.com>
2023-11-16 15:14:42 +0600

    nect condig

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-// module.exports = {
-//     nextConfig,
-//     images: {
-//         domains: ['images.unsplash.com'], // Add any other domains for external images
-//     },
-// }
-
-
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    /* config options here */
-  }
-   
-  module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    images: {
+        domains: ['images.unsplash.com'], // Add any other domains for external images
+    },
+}
